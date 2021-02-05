baa105de781e8b9827ff57278ef21c62f22fe012
rafenasir <rafenasir@gmail.com>
2021-02-05 01:46:18 +0100

    Created CMS HTML and CSS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-.
+let titleNews = document.getElementById("newsTitle").value;
+let detailNews = document.getElementById("newsBody").value;
+
+
+function console() {
+    console.log(detailNews);
+}
