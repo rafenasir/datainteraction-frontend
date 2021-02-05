f841c48a45ee6f4f08cd95bb368e3642f051375f
rafenasir <rafenasir@gmail.com>
2021-02-05 15:50:46 +0100

    Post function is working and returning the data

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-let titleNews = document.getElementById("newsTitle").value;
-let detailNews = document.getElementById("newsBody").value;
+// let titleNews = document.getElementById("newsTitle").value;
 
 
-function console() {
-    console.log(detailNews);
+// let detailNews = document.getElementById("newsBody").value;
+
+
+function runtheNews() {
+
+}
+
+
+function postNews() {
+    let titleNews = document.getElementById("newsTitle").value;
+    let detailNews = document.getElementById("newsBody").value;
+    console.log("Title of the News is " + titleNews);
+    console.log("Detai of the News Is " + detailNews);
+
+    fetch("http://localhost:3000/api/news", {
+            method: "POST",
+            body: JSON.stringify({
+                title: titleNews,
+                body: detailNews,
+
+            }),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+                "accept": "application/json"
+                    // charset=UTF-8'
+            }
+
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
