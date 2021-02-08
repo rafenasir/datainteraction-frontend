80bb4a9bf060df3e9432ca315596938103846ac2
rafenasir <rafenasir@gmail.com>
2021-02-08 01:21:35 +0100

    Generated JS to auto update the HTML

--- news.js
+++ news.js
@@ -1,7 +1,69 @@
-function getNews() {
-    fetch('http://localhost:3000/api/news')
-        .then(response => response.json())
-        .then(json => console.log(json))
+// document.write(`\
+// <div id = "news" class = "news col-sm-4">\
+// <h3 class = "news-title">Title</h3>\
+// <p class = "news-body">Body</p>\
+// <p class = "date-modified">Date</p>\
+// </div>\
+// `)
+
+let newsDiv = "<div id ='news' class ='news col-sm-4 col-bg-4'> <h3 id = 'news-title'></h3> <p class = 'news-body'> </p> <p class = 'date-modified'> </p></div>";
+
+async function getNews() {
+    let data = await fetch('http://localhost:3000/api/news')
+    let news = await data.json();
+    console.log(news);
+    createnewsPage(news);
+    return news
+
 }
+
+function createnewsPage(news) {
+    console.log("entered the function")
+    console.log(news);
+    for (i = 0; i < 4; i++) {
+        console.log("entered the loop");
+        let container = document.getElementById("news-all");
+        container.insertAdjacentHTML("beforeend", newsDiv);
+        document.getElementById("news-title").innerHTML = ;
+        console.log(i);
+        // let div = document.createElement("div");
+        // div.className = "news col-md-4 col-bg-4";
+        // container.appendChild(div);
+        // console.log("div created")
+        // let newsTitle = document.createElement("h3");
+        // newsTitle.className = "news-title";
+        // div.appendChild(newsTitle);
+        // document.getElementsByClassName("news-title").innerHTML = "News Title";
+        // console.log("Title h3 created")
+        // let bodyNews = document.createElement("p");
+        // bodyNews.className = "body-news";
+        // div.appendChild("bodyNews");
+        // document.getElementsByClassName("body-news").innerHTML = "Body News";
+        // let timeUpdated = document.createElement("p");
+        // timeUpdated.className.add("date-modified");
+        // div.appendChild("timeUpdated");
+        // document.getElementsByClassName("date-modified").innerHTML = "date Modified";
+
+    }
+
+    //     // for (i = 0; i > news.length; i++) {
+    // let newsContainer = document.getElementById("news-all");
+    // let newNews = document.write("<div><h4>Title News</h4><p>This is dynamic HTML generated</p></div>");
+    // newsContainer.appendChild(newNews);
+    // console.log(newNews);
+
+}
+
+// loop ()
+
+// document.create div h3 title p body 
+
+// document.getElementById("title").innerHTML = news[i].title;
+// document.getElementById("sample").innerHTML = news[0].body;
+// document.getElementById("sample").innerHTML = news[0].body;
+// document.getElementById("sample").innerHTML = news[0].body;
+
+
+
 
 getNews();
